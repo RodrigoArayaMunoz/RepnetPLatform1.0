80e127bc10b51c46445432260d59197f9183556f
Rodrigo <rodrigoarayamunoz.infor@gmail.com>
2026-03-31 15:35:10 -0300

    - Incluir login en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,25 @@ import Login from "./frontend/pages/Login";
 
 export default function App() {
   return (
-    <Login></Login>
+    //<Login></Login>
+      <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/compatibilidades/carga-masiva"
+            element={<Compatibilidades />}
+          />
+          <Route
+            path="/compatibilidades/no-compatibilidades"
+            element={<NoCompatibilidades />}
+          />
+          <Route
+            path="/actualizaciones/precios-stock"
+            element={<PreciosStock />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
