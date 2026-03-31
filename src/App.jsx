dd7bc84dd16ca7dbc530265c28453399e6a6f3eb
Rodrigo <rodrigoarayamunoz.infor@gmail.com>
2026-03-31 01:18:40 -0300

    - Nueva pantalla de login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,10 @@ import Home from "../src/frontend/pages/Home";
 import Compatibilidades from "./frontend/pages/CompatibilitiesUpload";
 import PreciosStock from "./frontend/pages/PriceStocksUploads";
 import NoCompatibilidades from "./frontend/pages/NoCompatibilities";
+import Login from "./frontend/pages/Login";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/compatibilidades/carga-masiva"
-            element={<Compatibilidades />}
-          />
-          <Route
-            path="/compatibilidades/no-compatibilidades"
-            element={<NoCompatibilidades />}
-          />
-          <Route
-            path="/actualizaciones/precios-stock"
-            element={<PreciosStock />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Login></Login>
   );
 }
